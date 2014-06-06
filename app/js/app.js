b0e110b8052e2883b87f943b7ead8bd6fdf3fcf3
Andrey Vyrvich <andry.virvich@gmail.com>
2014-06-06 15:00:20 +0700

    add option to compile app

--- app/js/app.js
+++ app/js/app.js
@@ -13,4 +13,20 @@ if (chrome.browserAction) {
 	chrome.browserAction.setBadgeBackgroundColor({
 		'color': '#3399F3'
 	});
+} else if (chrome.app && chrome.app.runtime) {
+	chrome.app.runtime.onLaunched.addListener(function() {
+	  chrome.app.window.create('index.html', {
+	    id: 'jira-app',
+	    bounds: {
+	      width: 1200,
+	      height: 800,
+	      left: 100,
+	      top: 100
+	    },
+	    minWidth: 400,
+	    minHeight: 400
+	  }, function(wnd) {
+	    wnd.contentWindow.app = app;
+	  });
+	});	
 }
