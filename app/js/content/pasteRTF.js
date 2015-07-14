f3f84ba6d05fff0c4ca0e1a6d949e6cd4793399a
e1i <eli.tabrisov@gmail.com>
2015-07-14 12:41:07 +0600

    fix paste plain text

--- app/js/content/pasteRTF.js
+++ app/js/content/pasteRTF.js
@@ -6,18 +6,25 @@ $(document).ready(function(){
             var pastedText = undefined;
             if (window.clipboardData && window.clipboardData.getData) { // IE
                 pastedText = window.clipboardData.getData('Text');
+                plnText = e.clipboardData.getData('text/plain');
             } else if (e.clipboardData && e.clipboardData.getData) {
                 pastedText = e.clipboardData.getData('text/html');
+                plnText = e.clipboardData.getData('text/plain');
             }
             //$("#target").append(pastedText);
-            var tmpstr='';
+            
+            if(pastedText==''|| pastedText==null){
+                $('textarea[name="comment"]').text(plnText);
+            } else{
+            var tmpstr='';    
             /*it takes needed tags:
                         <b><u><i>
              replace them to jira-formate and then clear other tags
             */
             tmpstr = pastedText.replace(/(<([^>]b+)>)/ig, "*").replace(/(<b>)/ig,"*").replace(/(<([^>]i+)>)/ig, "_").replace(/(<i>)/ig,"_").replace(/(<([^>]u+)>)/ig, "+").replace(/(<u>)/ig,"+").replace(/(<([^>]+)>)/ig,"");
-            //console.log(tmpstr);
-            $('textarea[name="comment"]').text(tmpstr);
+            console.log(tmpstr);
+                $('textarea[name="comment"]').text(tmpstr);
+            };
         });
     });
 });
